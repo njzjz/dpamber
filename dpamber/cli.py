d0c7acaa13eb6c2a188ee04be3d2fcef51780c97
Jinzhe Zeng <jinzhe.zeng@rutgers.edu>
2021-04-05 20:10:50 -0400

    update correction

--- dpamber/cli.py
+++ dpamber/cli.py
@@ -1,12 +1,13 @@
 import argparse
 
 from .model_devi import run as model_devi_run
+from .corr import run as corr_run
 
 def run():
         parser = argparse.ArgumentParser()
         subparsers = parser.add_subparsers()
         parser_model_devi = subparsers.add_parser(
-                "devi", help="Generating initial data for surface systems.")
+                "devi", help="Calculate model deviations.")
         parser_model_devi.add_argument('--models', type=str, nargs='+',
                                 help="models")
         parser_model_devi.add_argument('--cutoff', type=float, 
@@ -16,5 +17,23 @@ def run():
         parser_model_devi.add_argument('--qm_region', type=str,default=":1",
                         help="qm_region")
         parser_model_devi.set_defaults(func=model_devi_run)
+
+"""
+    get_amber_fp(cutoff=args.cutoff,
+                 parmfile=args.parmfile,
+                 ll=args.ll,
+                 hl=args.hl,
+                 target=args.target,
+                 out=args.out,
+                 """
+        parser_corr = subparsers.add_parser(
+                "corr", help="Generate systems for DPRc.")
+        parser_corr.add_argument('--cutoff', type=float, 
+                                help="cutoff")
+        parser_corr.add_argument('--parm7_file', type=str,default="qmmm.parm7",
+                        help="parm7_file")
+        parser_corr.add_argument('--qm_region', type=str,default=":1",
+                        help="qm_region")
+        parser_corr.set_defaults(func=model_devi_run)
         args = parser.parse_args()
         args.func(args)
--- dpamber/corr.py
+++ dpamber/corr.py
@@ -1,35 +1,21 @@
 import dpdata
 import numpy as np
-#from multiprocessing import Pool
-
-def read_traj(prefix, nc=None, frc_suffix=None):
-    s_ll = dpdata.LabeledSystem(
-        ll, mdfrc_file=ll+".frc.nc", fmt='amber/md', use_element_symbols="(%s)&!%s" % (target, ep))
-    # remove EP in QM region
-    s_ll = s_ll.pick_by_amber_mask(
-        ll+".parm7", ":* & !((%s)&%s)" % (target, ep))
 
 
 def get_amber_fp(cutoff: float,
                  parmfile: str,
                  ll: str,
                  hl: str,
-                 type_map: list,
                  target: str = ":1",
-                 out: str = "dataset",
-                 nc: str = None,
-                 frc_suffix: str = None):
+                 out: str = "dataset"):
     ms = dpdata.MultiSystems()
     ep = r'@%EP'
     interactwith = "(%s)<:%f&!%s" % (target, cutoff, ep)
 
     s_ll = dpdata.LabeledSystem(
-        ll, mdfrc_file=ll+".frc.nc", fmt='amber/md', use_element_symbols="(%s)&!%s" % (target, ep))
-    # remove EP in QM region
-    s_ll = s_ll.pick_by_amber_mask(
-        ll+".parm7", ":* & !((%s)&%s)" % (target, ep))
+        ll, fmt='amber/md', use_element_symbols=target)
     s_hl = dpdata.LabeledSystem(
-        hl, mdfrc_file=hl+".frc.nc", fmt='amber/md', use_element_symbols=target)
+        hl, fmt='amber/md', use_element_symbols=target)
     s_corr = s_ll.correction(s_hl)
     s_corr = s_corr.pick_by_amber_mask(
         hl+".parm7", interactwith, pass_coords=True)
@@ -37,5 +23,14 @@ def get_amber_fp(cutoff: float,
         ss = ss.remove_atom_names('EP')
         ms.append(ss)
 
-    ms.to_deepmd_npy(out, set_size=999999)
+    ms.to_deepmd_npy(out)
 
+
+def run(args):
+    get_amber_fp(cutoff=args.cutoff,
+                 parmfile=args.parmfile,
+                 ll=args.ll,
+                 hl=args.hl,
+                 target=args.qm_region,
+                 out=args.out,
+                 )
