218a1b597d4f46adf3615746db9032590d9f39cd
Jinzhe Zeng <jinzhe.zeng@rutgers.edu>
2021-08-06 18:04:43 -0400

    v0.0.8

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name = "dpamber",
-    version = "0.0.7",
+    version = "0.0.8",
     install_requires = [
         "numpy",
         "dpdata[amber]>=0.2.1",
