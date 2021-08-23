0c1259d94029ff0254d1728067f50b67a0dee656
Jinzhe Zeng <jinzhe.zeng@rutgers.edu>
2021-08-23 17:53:42 -0400

    bump version to 0.1.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name = "dpamber",
-    version = "0.0.8",
+    version = "0.1.0",
     install_requires = [
         "numpy",
         "dpdata[amber]>=0.2.1",
