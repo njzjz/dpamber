40006866ebf694b1df2e948727ca70dc86ee388e
Jinzhe Zeng <jinzhe.zeng@rutgers.edu>
2023-07-24 16:12:41 -0400

    fix arg
    Signed-off-by: Jinzhe Zeng <jinzhe.zeng@rutgers.edu>

--- dpamber/drdq.py
+++ dpamber/drdq.py
@@ -2,6 +2,7 @@ import numpy as np
 from dpdata.data_type import Axis, DataType, register_data_type
 
 register_data_type(
-    DataType("drdq", np.ndarray, (Axis.NFRAMES, Axis.NATOMS, 3, -1), required=False)
+    DataType("drdq", np.ndarray, (Axis.NFRAMES, Axis.NATOMS, 3, -1), required=False),
+    labeled=False,
 )
 ep = None
